5b581f227905e407626e2b0e46e15435fb7e6195
Dharmendra Pal <dharmendrpal@gmail.com>
2024-11-18 22:51:45 +0530

    new commit

--- src/test/java/com/DKP/extentlisteners/ExtentManager.java
+++ src/test/java/com/DKP/extentlisteners/ExtentManager.java
@@ -9,14 +9,8 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 public class ExtentManager {
 
 	private static ExtentReports extent;
-	
-	
-	
-
 	    public static ExtentReports createInstance(String fileName) {
 	        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(fileName);
-	       
-	        
 	        htmlReporter.config().setTheme(Theme.STANDARD);
 	        htmlReporter.config().setDocumentTitle(fileName);
 	        htmlReporter.config().setEncoding("utf-8");
